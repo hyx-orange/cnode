8cb10267d7927d482c7626a36da9f8d82a91fc6a
Your Name <you@example.com>
2019-12-16 13:50:19 +0800

    first commit

---
+++ cnode/src/views/Home.vue
@@ -0,0 +1,32 @@
+﻿<template>
+    <div>
+        Home
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        components: {},
+        props: {},
+        data() {
+            return {}
+        },
+        methods: {},
+        mounted() {
+
+        },
+        created() {
+
+        },
+        filters: {},
+        computed: {},
+        watch: {},
+        directives: {}
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
